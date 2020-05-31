dd813a20a908b733a1598118bf12f8aeb1d28420
Sumit Deo <sumit.deo@outlook.com>
2020-05-31 18:24:55 -0400

    StreamsExample1

---
+++ LambdaBasics/src/com/sdeo/unit3/StreamsExample1.java
@@ -0,0 +1,29 @@
+package com.sdeo.unit3;
+
+import com.sdeo.common.Person;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Sumit Deo
+ */
+public class StreamsExample1 {
+
+    public static void main(String[] args) {
+
+        List<Person> people = Arrays.asList(
+                new Person("Sumit", "Deo", 32),
+                new Person("Sneha", "Pimpley", 31),
+                new Person("Apoorva", "Yeragi", 30),
+                new Person("Sampada", "Aloni", 32),
+                new Person("Ujjwal", "Garad", 30),
+                new Person("Shruti", "Mandke", 28),
+                new Person("Pratiksha", "Kulkarni", 30)
+        );
+
+        people.stream()
+                .filter( person -> person.getFirstName().startsWith("S"))
+                .forEach( p -> System.out.println(p.getFirstName()));
+    }
+}
